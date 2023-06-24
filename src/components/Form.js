72c4e916279ac14e5e44709b0f0a4e166b5d9604
Sandeep K K <sandeepdotcode@gmail.com>
2023-06-24 14:10:01 +0530

    Adjust form styling and Add buttons

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,18 +2,36 @@ import { Component } from "react";
 
 class Personal extends Component {
   render() {
+    const summaryString = `As a Principal Software Engineer, I excel in designing and developing robust and scalable software solutions ...`;
+
     return (
       <>
         <div className="personal-input-container">
           <label><span>Full Name</span><input type="text" placeholder="John Seed"></input></label>
-          <label><span>Job Title<span> optional</span></span><input type="text"></input></label>
-          <label><span>Email</span><input type="email"></input></label>
-          <label><span>Phone<span> not recommended</span></span><input type="text"></input></label>
+          <label><span>Job Title<span> optional</span></span>
+            <input type="text" placeholder="Principal Software Engineer"></input>
+          </label>
+          <label><span>Email</span><input type="email" placeholder="johnseed@xyz.com"></input></label>
+          <label><span>Phone<span> not recommended</span></span><input type="text" placeholder="944 704 8000"></input></label>
         </div>
         <label className="summary-label"><span>Summary<span> not recommended</span></span>
-          <textarea></textarea>
+          <textarea placeholder={summaryString}></textarea>
         </label>
       </>
+    )
+  }
+}
+
+class FormTitle extends Component {
+  render() {
+    return (
+      <div className="form-title-div">
+        <h3>Personal Details</h3>
+        <div className="next-btn-div">
+          <button type="button" className="prev-btn">Back</button>
+          <button type="button" className="next-btn">Next</button>
+        </div>
+      </div>
     )
   }
 }
@@ -22,7 +40,7 @@ class Form extends Component {
   render() {
     return (
       <div className="form-container">
-        <h3>Personal Details</h3>
+        <FormTitle />
         <form className="form">
           <Personal />
         </form>
